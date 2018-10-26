015ce51575b218237d262cd6f498805c2abd3564
masila <abelmasila@gmail.com>
2018-10-26 11:20:54 +0300

    created a mutation and a query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    //create a dog
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = Mutations;
