aa86471f6bc6ca1f3a3d63f88ea15ca9fcbcdd6e
masila <abelmasila@gmail.com>
2018-10-26 12:23:27 +0300

    Added resolvers and updated item schema

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,15 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    //create a dog
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    //TODO: check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
   }
 };
 
